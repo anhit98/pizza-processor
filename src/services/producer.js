b3b5d3f19f4a50305e4caa5b6768e689f9f3a7b4
Anh Do Thi Ngoc <anh.dothingoc@imac-0021.pyco.vn>
2019-06-03 18:09:53 +0700

    doanh

--- src/services/producer.js
+++ src/services/producer.js
@@ -10,37 +10,41 @@ const kafkaConf = {
   "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
   "debug": "generic,broker,security"
 };
-
-const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
-const topic = `${prefix}.test`;
-const producer = new Kafka.Producer(kafkaConf);
-const maxMessages = 20;
-
-const genMessage = i => new Buffer(`Kafka example, message number ${i}`);
-producer.on('delivery-report', function(err, report) {
-  if (err) {
-      console.error('Delivery report: Failed sending message ' + JSON.stringify(report));
-      console.error(err);
-      // We could retry sending the message or store it locally
-  } else {
-      console.log('Message produced, offset: ' + report.offset);
+var producer = new Kafka.Producer(kafkaConf);
+ 
+// Connect to the broker manually
+producer.connect();
+ 
+// Wait for the ready event before proceeding
+producer.on('ready', function() {
+  try {
+    producer.produce(
+      // Topic to send the message to
+      'topic',
+      // optionally we can manually specify a partition for the message
+      // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
+      null,
+      // Message to send. Must be a buffer
+      Buffer.from('Awesome message'),
+      // for keyed messages, we also specify the key - note that this field is optional
+      'Stormwind',
+      // you can send a timestamp here. If your broker version supports it,
+      // it will get added. Otherwise, we default to 0
+      Date.now(),
+      // you can send an opaque token here, which gets passed along
+      // to your delivery reports
+    );
+    console.log("gfrdgbv");
+  } catch (err) {
+    console.error('A problem occurred when sending our message');
+    console.error(err);
   }
-});
-producer.on("ready", function(arg) {
-  console.log(`producer ${arg.name} ready.`);
-  Buffer.from(JSON.stringify({_id: "id", status: "status"}))
-});
-// Register delivery report listener
-
-producer.on("disconnected", function(arg) {
-  process.exit();
-});
-
-producer.on('event.error', function(err) {
-  console.error(err);
-  process.exit(1);
 });
 producer.on('event.log', function(log) {
   console.log(log);
 });
-producer.connect();
+// Any errors we encounter, including connection errors
+producer.on('event.error', function(err) {
+  console.error('Error from producer');
+  console.error(err);
+})
